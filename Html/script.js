9f557ce33af866b7abaa677bad71f1a9c1da034f
Benjamin554 <Benjamim203224282@gmail.com>
2024-11-26 20:02:17 +0200

    update

---
+++ Html/script.js
@@ -0,0 +1,12 @@
+function showAlert() {
+    alert('Hello! Welcome to my website!');
+}
+
+function changeBackgroundColor() {
+    // בוחר צבע אקראי
+    const colors = ['#FF5733', '#33FF57', '#3357FF', '#F0E68C', '#FF69B4'];
+    const randomColor = colors[Math.floor(Math.random() * colors.length)];
+    
+    // משנה את צבע הרקע של הדף
+    document.body.style.backgroundColor = randomColor;
+}
